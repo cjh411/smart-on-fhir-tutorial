2b12308157deb0bab2a78695d6df331d5b947ccd
chrishedenberg <cjh411@gmail.com>
2020-02-09 10:13:11 -0500

    re add height

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -49,7 +49,7 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(weight[0]);//getQuantityValueAndUnit(height[0]);
+          p.height = getQuantityValueAndUnit(height[0]);
           p.weight = getQuantityValueAndUnit(weight[0]);
 
           if (typeof systolicbp != 'undefined')  {
